cf25474e45fbc56783a632af1ade239d5d0dcc73
Fifv <fifvzheyli@gmail.com>
2026-05-02 14:21:34 +0800

    update;

--- src/try39-webgl.tsx
+++ src/try39-webgl.tsx
@@ -1,7 +1,25 @@
-import { useState } from "react"
+import { clsx } from "clsx"
+import { useEffect, useRef, useState } from "react"
 export default function App() {
     const [count, setCount] = useState(0)
+    const refCanvas = useRef<HTMLCanvasElement>(null!)
+    useEffect(() => {
+        const canvas = refCanvas.current
+        const gl = canvas.getContext("webgl2")
+        if (gl === null) {
+            alert('webgl2 not supported')
+            throw "?"
+        }
+        gl.clearColor(0.5, 1.0, 0.7, 1.0)
+        gl.clear(gl.COLOR_BUFFER_BIT)
+    }, [])
     return (
-        <div></div>
+        <div>
+            <canvas ref={ refCanvas } className={ clsx(
+                'm-4 w-3/4 h-[80vh] outline outline-dashed outline-amber-300/50',
+            ) }>
+
+            </canvas>
+        </div>
     )
 }
