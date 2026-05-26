47231f39b9203e7fe86247883e1d0326f5f3ec6b
Fifv <fifvzheyli@gmail.com>
2026-05-26 17:00:40 +0800

    update;

---
+++ src/try40-mediabunny.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useState } from "react"
+import { Input, ReadableStreamSource, ALL_FORMATS, UrlSource, MATROSKA, Output, MP4, Mp4OutputFormat, Target, StreamTarget, BufferTarget, Conversion } from 'mediabunny'
+export default function App() {
+    const [count, setCount] = useState(0)
+
+    const refVideo = useRef<HTMLVideoElement>(null)
+    useEffect(() => {
+        (async () => {
+            // const bodyStream = await fetch('./test.mkv').then((x) => x.body)
+            // console.log(bodyStream)
+            const input = new Input({
+                formats: [MATROSKA],
+                // source: new ReadableStreamSource(bodyStream),
+                source: new UrlSource('./test.mkv'),
+            })
+            console.log(input)
+            const duration = await input.computeDuration()
+            console.log('duration:', duration)
+            const allTracks = await input.getTracks()
+            console.log('allTracks:', allTracks)
+
+
+            const output = new Output({
+                format: new Mp4OutputFormat(),
+                target: new BufferTarget(),
+            })
+            const conversion = await Conversion.init({
+                input: input,
+                output: output,
+            })
+            console.log('conversion.execute');
+            console.time('conversion.execute');
+            await conversion.execute()
+            console.timeEnd('conversion.execute');
+            if (output.target.buffer) {
+                console.log(output.target.buffer)
+                const blob = new Blob([output.target.buffer], {
+                    type: await output.getMimeType(),
+                })
+                const video = refVideo.current
+                if (video) {
+                    video.src = URL.createObjectURL(blob)
+                    // await video.play()
+                }
+            }
+            // output.start()
+            // output.addVideoTrack(await input.getPrimaryVideoTrack())
+            // console.log(output)
+            // if (output.target.buffer) {
+            //     const blob = new Blob([output.target.buffer])
+            //     console.log(blob)
+            // }
+        })()
+    }, [])
+    return (
+        <div>
+            <video ref={ refVideo } controls className="h-72 w-128" />
+        </div>
+    )
+}
