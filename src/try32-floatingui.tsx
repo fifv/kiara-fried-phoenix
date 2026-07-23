0fbafd273280b4c70f31f8492ae42daf166d1390
Fifv <fifvzheyli@gmail.com>
2026-07-23 10:35:45 +0800

    update;

--- src/try32-floatingui.tsx
+++ src/try32-floatingui.tsx
@@ -1,30 +1,61 @@
 import { useState } from "react"
-import { autoUpdate, useFloating } from '@floating-ui/react'
+import { arrow, autoUpdate, flip, offset, shift, useFloating } from '@floating-ui/react'
 import { range } from "lodash-es"
 import { clsx } from "clsx"
 export default function App() {
     return (<>
-        {
-            range(100).map((item) => <div key={ item }>
-                <ButtonWithTooltip></ButtonWithTooltip>
-            </div>)
-        }
+        <div className={ clsx(
+            'w-96 h-96 outline m-12 overflow-auto relative',
+        ) }>
+            <div className="h-24 w-160 bg-amber-800 "></div>
+            <div className="flex flex-nowrap">
+                <div className="w-36 bg-blue-800"></div>
+                <ButtonWithTooltip />
+            </div>
+            <div className="h-96 bg-amber-800"></div>
+        </div>
     </>)
 
 }
 
 function ButtonWithTooltip() {
     const [count, setCount] = useState(0)
-    const { refs, floatingStyles } = useFloating({
+    const [arrowElement, setArrowElement] = useState<HTMLDivElement | null>(null)
+    const {
+        refs: { setReference, setFloating },
+        floatingStyles,
+        middlewareData,
+        placement,
+    } = useFloating({
         whileElementsMounted: autoUpdate,
+        placement: "top",
+        middleware: [
+            offset(6),
+            flip(),
+            shift({ padding: 5 }),
+            arrow({ element: arrowElement })
+        ]
     })
+
+    const staticSide = { top: "bottom", right: "left", bottom: "top", left: "right" }[placement.split("-")[0]]!
+
     return (
         <div className={ clsx(
-            ' outline-red-400',
+            ' outline-red-400 relative',
         ) }>
-            <button ref={ refs.setReference }>Click Me</button>
-            <div ref={ refs.setFloating } style={ floatingStyles }>what's this?</div>
+            <button ref={ setReference } className="">Me</button>
+            <div ref={ setFloating } style={ floatingStyles } className="w-max max-w-72">
+                what's this? Good Morning!
+                <div ref={ setArrowElement } className={ clsx(
+                    'Arrow absolute bg-red-400 w-1 h-1',
+                ) } style={ {
+                    // [placement]: middlewareData.arrow?.x,
+                    // [placement]: middlewareData.arrow?.y,
+                    'left': middlewareData.arrow?.x,
+                    'top': middlewareData.arrow?.y,
+                    [staticSide]: `${-0}px`
+                } }></div>
+            </div>
         </div>
     )
 }
-
